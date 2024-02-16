11c63d87771864af79a19bb2dec16be84217efaa
Morgan Goeller <morgango@gmail.com>
2024-02-16 16:31:04 -0600

    Added guardrails

---
+++ prompting/guardrails/chat-basic.py
@@ -0,0 +1,21 @@
+from nemoguardrails import LLMRails, RailsConfig
+from workshops_common import set_local_directory
+from icecream import ic
+
+# make sure python is looking in the right spot for the files we need.
+set_local_directory()
+
+config_path = './config-with-flow'
+
+# load the configuration
+config = RailsConfig.from_path(config_path)
+rails = LLMRails(config=config)
+
+
+# send a message to the model
+response = rails.generate(messages=[{
+    "role": "user",
+    "content": "Hello!"
+}])
+
+ic(response)
